a59c6287eb9f5dc1c7d7316e314ec91e9f1c18ae
Barış Aslan <baris.aslan100@gmail.com>
2021-06-14 23:53:08 +0300

    useFormik 2

--- src/pages/AddJobPosting.jsx
+++ src/pages/AddJobPosting.jsx
@@ -7,7 +7,7 @@ import WorkTimeService from "../services/workTimeService";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import JobPostingService from "../services/jobPostingService";
-import { Form ,Button} from "semantic-ui-react";
+import { Form, Button } from "semantic-ui-react";
 
 export default function AddJobPosting() {
   let jobPostingService = new JobPostingService();
@@ -59,27 +59,15 @@ export default function AddJobPosting() {
       workTypeId: "",
       workTimeId: "",
       deadline: "",
-      jobDescription: ""
+      jobDescription: "",
     },
-    validationSchema: Yup.object({
-      jobPositionId: Yup.number().required("Bir pozisyon seçiniz!"),
-      cityId: Yup.number().required("Bir şehir seçiniz!"),
-      numberOfOpenPosition: Yup.number().required(
-        "Açık pozisyon sayısı girin!"
-      ),
-      minSalary: Yup.number().required("Minimum maaş skalası giriniz!"),
-      maxSalary: Yup.number().required("Maksimum maaş skalası giriniz!"),
-      workTypeId: Yup.number().required("Bir çalışma türü seçiniz!"),
-      workTimeId: Yup.number().required("Bir çalışma zamanı seçiniz!"),
-      deadline: Yup.date().required("Bitiş tarihini giriniz!"),
-      jobDescription: Yup.string().required("Açıklama giriniz!"),
-    }),
+
     onSubmit: (values) => {
       values.employerId = 10;
       jobPostingService
         .AddJobPosting(values)
         .then((result) => console.log(result.data.data));
-    }
+    },
   });
 
   const jobPositonOptions = jobPositions.map((jobPosition) => ({
@@ -91,19 +79,19 @@ export default function AddJobPosting() {
   const cityOptions = cities.map((city) => ({
     key: city.cityId,
     text: city.name,
-    value: city.cityId
+    value: city.cityId,
   }));
 
   const workTypeOptions = workTypes.map((workType) => ({
     key: workType.workTypeId,
     text: workType.workType,
-    value: workType.workTypeId
+    value: workType.workTypeId,
   }));
 
   const workTimeOptions = workTimes.map((workTime) => ({
     key: workTime.workTimeId,
     text: workTime.workTime,
-    value: workTime.workTimeId
+    value: workTime.workTimeId,
   }));
 
   return (
@@ -150,7 +138,7 @@ export default function AddJobPosting() {
             type="number"
             onChange={handleChange}
             onBlur={handleBlur}
-            value={values.minSalar}
+            value={values.minSalary}
             fluid
             label="Minimum Maaş Skalası"
             placeholder="Minimum Maaş Skalası"
@@ -213,7 +201,9 @@ export default function AddJobPosting() {
           ></Form.TextArea>
         </Form.Group>
 
-        <Button type="submit" type="button" disabled={!dirty || isSubmitting} primary>YAYINLA</Button>
+        <Button type="submit" primary>
+          YAYINLA
+        </Button>
       </Form>
     </div>
   );
